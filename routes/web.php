8e015c90f92ae328322397203af1d9c1ae5ee00b
Edtlsoft <edwardlopez282@gmail.com>
2020-08-07 22:32:00 -0500

    [add] Model, Migration and Controller for Status

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('/statuses', 'StatusController@store')->name('statuses.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// \\
--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -9,17 +9,21 @@ use Tests\TestCase;
 
 class CreateStatusTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
     public function a_authenticated_user_can_create_statuses()
     {
+        $this->withoutExceptionHandling();
+
         // 1. Given => Given a authenticated user
-        $user = factory(User::class)->create(['edward@edtlsoft.com']);
+        $user = factory(User::class)->create(['email' => 'edward@edtlsoft.com']);
         $this->actingAs($user);
 
         // 2. When  => When you make a post request to status
-        $this->post(route('status.store'), ['body' => 'My first status']);
+        $this->post(route('statuses.store'), ['body' => 'My first status']);
 
         // 3. Then  => Then I see a new status in the database
         $this->assertDatabaseHas('statuses', ['body' => 'My first status']);
