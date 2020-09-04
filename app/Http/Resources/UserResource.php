f6698a95558a25f79dc515c6b434054fdc8fcd58
Edtlsoft <edwardlopez2824@gmail.com>
2020-09-03 21:52:37 -0500

    [add] User resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_name' => $this->resource->name,
+            'user_avatar' => $this->resource->avatar(),
+            'user_link' => $this->resource->link(),
+        ];
+    }
+}
