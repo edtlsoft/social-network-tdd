bbfd46655f868801fb910fd4c1c7340149b1789a
Edtlsoft <edwardlopez2824@gmail.com>
2020-09-06 22:30:46 -0500

    [add] Design to the user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,11 +54,10 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">Profile</a>
                                     <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="#" onclick="document.getElementById('logout').submit()">
                                         Logout
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,1 +1,25 @@
-{{ $user->username  }}
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card border-0 bg-light shadow-sm">
+                    <img src="{{ $user->avatar() }}" alt="" class="card-img-top">
+                    <div class="card-body">
+                        {{ $user->username }}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="card card border-0 bg-light shadow-sm">
+                    <div class="card-body">
+                        Content
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
